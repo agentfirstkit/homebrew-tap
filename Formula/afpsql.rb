8b87cd3342c4346cf7a8861837d9026ef249f59a
agentfirstkit <support@agentfirstkit.com>
2026-07-29 16:03:07 +0800

    afpsql v0.8.2

--- Formula/afpsql.rb
+++ Formula/afpsql.rb
@@ -1,24 +1,24 @@
 class Afpsql < Formula
   desc "A PostgreSQL interface for AI agents: reliable, structured, explicit, and read-only by default."
   homepage "https://github.com/agentfirstkit/agent-first-psql"
-  version "0.8.1"
+  version "0.8.2"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/agentfirstkit/agent-first-psql/releases/download/v0.8.1/afpsql-v0.8.1-aarch64-apple-darwin.tar.gz"
-      sha256 "b4e202c6a944295e93f6512ef7dec6af596a20b6794cb553611b925c949bf443"
+      url "https://github.com/agentfirstkit/agent-first-psql/releases/download/v0.8.2/afpsql-v0.8.2-aarch64-apple-darwin.tar.gz"
+      sha256 "b5ca70412974650a8ef4f1bc9ffa59b2fbfd03ee68087410d08806f481ee26a2"
     end
     on_intel do
-      url "https://github.com/agentfirstkit/agent-first-psql/releases/download/v0.8.1/afpsql-v0.8.1-x86_64-apple-darwin.tar.gz"
-      sha256 "13f95bff5c8fc308eec11a3cb99d4b04a66706c9fdc9b0c085ac28eb72f0fa31"
+      url "https://github.com/agentfirstkit/agent-first-psql/releases/download/v0.8.2/afpsql-v0.8.2-x86_64-apple-darwin.tar.gz"
+      sha256 "17670688ac929d2285e5bf3bb694043e6af9be6500fb1f90e2f2ee4cc24456b0"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/agentfirstkit/agent-first-psql/releases/download/v0.8.1/afpsql-v0.8.1-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "2a8ba1c6ab01341c5e843ea569f10e8c6ee52358709158515283635e189f8110"
+      url "https://github.com/agentfirstkit/agent-first-psql/releases/download/v0.8.2/afpsql-v0.8.2-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "6c2911fece19a9792628c13e0519c91ab6c97495ed97ff62d6cf6fcbb9931d13"
     end
   end
 
