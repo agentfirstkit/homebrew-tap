6ce75c7c8a3fe8f448eaad914eb47caeea5f201c
agentfirstkit <support@agentfirstkit.com>
2026-09-13 01:59:49 +1200

    afhttp v0.14.0 from db242184cbe1244988920e26a59b610c1ced3ed4

--- Formula/afhttp.rb
+++ Formula/afhttp.rb
@@ -5,23 +5,23 @@ class Afhttp < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/agentfirstkit/agent-first-http/releases/download/v0.13.2/afhttp-v0.13.2-aarch64-apple-darwin.tar.gz"
-      sha256 "1f8775ed12347f342ad534b1b0b2dbcd8c9901b531f4c7298004910ed3e2a69a"
+      url "https://github.com/agentfirstkit/agent-first-http/releases/download/v0.14.0/afhttp-v0.14.0-aarch64-apple-darwin.tar.gz"
+      sha256 "75ecfc28d2ceb5fae937f06da00f96dc99cac7e2d1a675acb6ef787c4dfbb3d7"
     end
     on_intel do
-      url "https://github.com/agentfirstkit/agent-first-http/releases/download/v0.13.2/afhttp-v0.13.2-x86_64-apple-darwin.tar.gz"
-      sha256 "95776245206e31eae25ba1ec83772b60f0496d94282affe4fa1e644d9428409a"
+      url "https://github.com/agentfirstkit/agent-first-http/releases/download/v0.14.0/afhttp-v0.14.0-x86_64-apple-darwin.tar.gz"
+      sha256 "c3e794ff62cf800f5ffb06b7832b220c31e4595484fdeb8c516465d7a77caa87"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/agentfirstkit/agent-first-http/releases/download/v0.13.2/afhttp-v0.13.2-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "d70e72a4d2698f401e4c2fd28de1b33e7a6a3fad185a2cd1413fd2308d29ef61"
+      url "https://github.com/agentfirstkit/agent-first-http/releases/download/v0.14.0/afhttp-v0.14.0-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "9f2683d1f6e908a4844d3895e1776fa5001ccffae67a0e9df7740cffd0d33be0"
     end
     on_intel do
-      url "https://github.com/agentfirstkit/agent-first-http/releases/download/v0.13.2/afhttp-v0.13.2-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "764174235de3628c58654ef8c23d04902e5c8fe24614fd23d72523b5e718f194"
+      url "https://github.com/agentfirstkit/agent-first-http/releases/download/v0.14.0/afhttp-v0.14.0-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "259f25646ef98f40043e3c80077ed10e619a082293e80db868256a5d8b8ba90e"
     end
   end
 
